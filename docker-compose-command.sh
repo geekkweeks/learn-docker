4783aba6bfc2b143a374476998c73952e2bb3ad0
alfan <zahgreat30@gmail.com>
2024-10-15 15:20:02 +0700

    first commit

---
+++ docker-compose-command.sh
@@ -0,0 +1,14 @@
+docker compose create
+docker compose start
+
+#melihat container
+docker compose ps
+
+#stop container
+docker compose stop
+
+#remove container
+docker compose down
+
+#menampilkan project docker compose yang sedang running
+docker compose ls
